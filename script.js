8c54dfeb8123fac8178861b9b68ea1df4adce19b
Sonja <sonjaljatkovic@gmail.com>
2023-09-10 10:37:35 -0400

    added for loop and updated html

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
+// <!-- Use class for "past", "present", and "future" to apply styles to the
+// time-block divs accordingly. The javascript will need to do this by
+// adding/removing these classes on each div by comparing the hour in the
+// id to the current hour. The html provided below is meant to be an example
+// demonstrating how the css provided can be leveraged to create the
+// desired layout and colors. The html below should be removed or updated
+// in the finished product. Remember to delete this comment once the
+// code is implemented.
+// -->
+
+
+// <!-- Example of a a present time block. The "present" class adds a red background color.
+// <div id="hour-10" class="row time-block past">
+//   <div class="col-2 col-md-1 hour text-center py-3">10AM</div>
+//   <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+//   <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+//     <i class="fas fa-save" aria-hidden="true"></i>
+//   </button>
+// </div>
+
+// Example of a future time block. The "future" class adds a green background color.
+// <div id="hour-11" class="row time-block past">
+//   <div class="col-2 col-md-1 hour text-center py-3">11AM</div>
+//   <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+//   <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+//     <i class="fas fa-save" aria-hidden="true"></i>
+//   </button>
+// </div> -->
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+// $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +49,23 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+// });
+
+
+var containerEl = $(".container")
+var rowEl = $(".time-block")
+var hourEl = $(".hour")
+
+$(function () {
+  
+  for (var i = 9; i <= 17; i++) {
+    var newRow = rowEl.clone();
+    newRow.attr("id", "hour-" + [i])
+    newRow.find(".hour").text(i);
+    containerEl.append(newRow);
+    
+  }
+  rowEl.remove();
+  
+
 });
