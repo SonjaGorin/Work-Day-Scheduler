eeb907fd7e9523740a94dbedb593567150b7265e
Sonja <sonjaljatkovic@gmail.com>
2023-09-12 11:50:26 -0400

    cleaned up the code

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-var containerEl = $(".container")
-var rowEl = $(".time-block")
-var hourEl = $(".hour")
-var currentDayEl = $("#currentDay")
-var ButtonEl = $(".saveBtn")
-var eventInputEl = (".description")
+var containerEl = $(".container");
+var rowEl = $(".time-block");
+var hourEl = $(".hour");
+var currentDayEl = $("#currentDay");
+var ButtonEl = $(".saveBtn");
+var eventInputEl = (".description");
   
 function renderCalendar() {
   for (var i = 9; i <= 17; i++) {
     var newRow = rowEl.clone();
     newRow.attr("id", "hour-" + [i]);
-    newRow.children().eq(1).attr("id", "text-" + [i])
-    newRow.children().eq(2).attr("id", "button-" + [i])
-    newRow.addClass("new-row")
-    var calendarTime = newRow.find(".hour")
+    newRow.children().eq(1).attr("id", "text-" + [i]);
+    newRow.children().eq(2).attr("id", "button-" + [i]);
+    newRow.addClass("new-row");
+    var calendarTime = newRow.find(".hour");
     if (i < 10) {
       calendarTime.text("0" + i + ":00");
     } else {
@@ -25,18 +25,18 @@ function renderCalendar() {
 
 function colorCalendar() { 
   $(".hour").each(function() {
-    var calendarTimeSplit = $(this).text().split(":")
+    var calendarTimeSplit = $(this).text().split(":");
     var hour = Number(calendarTimeSplit[0]);
     var currentHour = Number(dayjs().format("HH"));
 
     if (hour < currentHour) {
-      $(this).parent().addClass("past")
+      $(this).parent().addClass("past");
     }
     if (hour === currentHour) {
-      $(this).parent().addClass("present")
+      $(this).parent().addClass("present");
     }
     if (hour > currentHour) {
-      $(this).parent().addClass("future")
+      $(this).parent().addClass("future");
     }
   })
 }
@@ -56,13 +56,13 @@ function updateCalendarEvents(event) {
   event.preventDefault();
 
   var buttonClicked = $(event.currentTarget);
-  buttonClicked.parent()
-  var buttonClickedParent = buttonClicked.parent()
-  var buttonClickedText = buttonClickedParent.children().eq(1).val()
-  var buttonClickedTextId = buttonClickedParent.children().eq(1).attr("id")
+  buttonClicked.parent();
+  var buttonClickedParent = buttonClicked.parent();
+  var buttonClickedText = buttonClickedParent.children().eq(1).val();
+  var buttonClickedTextId = buttonClickedParent.children().eq(1).attr("id");
 
-  calendarEvents[buttonClickedTextId] = buttonClickedText
-  storeCalendarEvents()
+  calendarEvents[buttonClickedTextId] = buttonClickedText;
+  storeCalendarEvents();
 }
 
 function loadCalendarEvents() {
